42fc82e18ac02faceda4539b486ba09717b2a99a
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-06-14 14:40:14 +0200

    New upstream version 2.3.0

--- assets/upload.js
+++ assets/upload.js
@@ -34,6 +34,9 @@ window.addEventListener("DOMContentLoaded", function() {
   });
 
   let file_upload = document.querySelector("input[type=file]");
+  let upload_list = document.createElement("dl");
+  file_upload.parentElement.appendChild(upload_list);
+
   file_upload.addEventListener("change", function() {
     for(let i = file_upload.files.length - 1; i >= 0; --i) {
       let file = file_upload.files[i];
@@ -43,6 +46,36 @@ window.addEventListener("DOMContentLoaded", function() {
 
   function upload_file(req_url, file) {
     ++remaining_files;
+
+    let filename_line = document.createElement("dt");
+    filename_line.innerText = file.name;
+    upload_list.appendChild(filename_line)
+
+    let progress_progress_label = document.createElement("label");
+    let progress_progress_desc  = document.createElement("span");
+    let progress_progress       = document.createElement("progress");
+    progress_progress.value = 0;
+    progress_progress_label.appendChild(progress_progress);
+    progress_progress_label.appendChild(document.createTextNode(" "));
+    progress_progress_desc.innerText = "pending";
+    progress_progress_label.appendChild(progress_progress_desc);
+
+    let progress_speed = document.createElement("span");
+    let progress_speed_text = document.createTextNode("?");
+    progress_speed.appendChild(progress_speed_text);
+    progress_speed.appendChild(document.createTextNode("/s"));
+
+    let progress_time = document.createElement("span");
+    progress_speed.appendChild(progress_time);
+
+    let progress_line     = document.createElement("dd");
+    progress_line.appendChild(progress_progress_label);
+    progress_line.appendChild(document.createTextNode(" "));
+    progress_line.appendChild(progress_speed);
+    progress_line.appendChild(document.createTextNode(" "));
+    progress_line.appendChild(progress_time);
+    upload_list.appendChild(progress_line)
+
     if(!file_upload_text) {
       file_upload_text = document.createTextNode(1);
       file_upload.parentNode.insertBefore(file_upload_text, file_upload.nextSibling); // insertafter
@@ -53,11 +86,43 @@ window.addEventListener("DOMContentLoaded", function() {
     request.addEventListener("loadend", function(e) {
       if(request.status >= 200 && request.status < 300) {
         if(!--remaining_files)
-         window.location.reload();
+          window.location.reload();
+
+        filename_line.remove();
+        progress_line.remove();
+
         file_upload_text.data = remaining_files;
-      } else
+      } else {
+        progress_line.innerText = request.response;
         file_upload.outerHTML = req_url + "<br />" + request.response;
+      }
     });
+
+    let start = 0;
+    let update = -1000;
+    request.upload.addEventListener("loadstart", function(e) {
+      start = e.timeStamp;
+    });
+    let prog = function(e) {
+      if(e.lengthComputable) {
+        progress_progress.value = event.loaded;
+        progress_progress.max   = event.total;
+
+        let elapsed = (e.timeStamp - start) / 1000; // s
+        if(elapsed > 0.1) {
+          let speed = event.loaded / elapsed;
+          progress_progress_desc.innerText = human_readable_size(event.loaded) + "/" + human_readable_size(event.total);
+          progress_speed_text.data         = human_readable_size(speed);
+          progress_time.innerText          = hms(elapsed) + "/" + hms(event.total / speed);
+        }
+      } else {
+        progress_progress.removeAttribute("value");
+        progress_progress_desc.innerText = "uploading";
+        request.upload.removeEventListener("progress", prog);
+      }
+    };
+    request.upload.addEventListener("progress", prog);
+
     request.open("PUT", req_url);
     if(file.lastModified)
       request.setRequestHeader("X-Last-Modified", file.lastModified);
@@ -89,5 +154,29 @@ window.addEventListener("DOMContentLoaded", function() {
       if(e.length)
         all_in_reader(reader, f);
     });
+  }
+
+  // Ported from util::HumanReadableSize
+  const LN_KIB = Math.LN2 * 10; // 1024f64.ln()
+  function human_readable_size(num) {
+    let exp = Math.min(Math.max(Math.trunc(Math.log(num) / LN_KIB), 0), 8);
+
+    let val = num / Math.pow(2, exp * 10);
+
+    return ((exp > 0) ? Math.round(val * 10) / 10 : Math.round(val)) + " "
+           + ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"][Math.trunc(Math.max(exp, 0))];
+  }
+
+  function hms(seconds) {
+    let h = Math.trunc(seconds / 3600);
+    let m = Math.trunc((seconds % 3600) / 60);
+    let s = Math.trunc(seconds % 60);
+
+    if(h)
+      return h + ":" + String(m).padStart(2, '0') + ":" + String(s).padStart(2, '0');
+    else if(m)
+      return m + ":" + String(s).padStart(2, '0');
+    else
+      return seconds.toFixed(1) + "s";
   }
 });
